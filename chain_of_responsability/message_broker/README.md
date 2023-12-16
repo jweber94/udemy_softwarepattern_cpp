95a7fe0796d1a1bdb8a021c5259d823fbcc1129b
Jens Weber <jweber1094@gmail.com>
2023-12-16 10:49:38 +0100

    Add message broker example and prepare for the homework assignment

--- chain_of_responsability/message_broker/README.md
+++ chain_of_responsability/message_broker/README.md
@@ -1,5 +1,13 @@
 # Message Broker Design Pattern
-+ It is a combination of the design patterns:
++ The event broker is a combination of the design patterns:
 	- Observer-Pattern
 	- Chain of Responsability
-	- Mediator
+	- Command Query Separation
+	- Mediator Pattern
+
+## Basic Idea
++ The Game object is the mediator that connects query objects with instance objects within the game by working as an observable (implemented with `boost::signals2`)
++ The instance objects hold the basic value (for attack and defence) and ask the game object for their member access (attack and defence) since these depend on modifyer objects that might be present within the game
++ The Modifyer objects connect to the signal member of the game and link a lambda to them which modifys values of the instances within the game whenever they are requested
++ The Query instance holds the modifyer adjusted values of the requested value
++ The main part of this message broker design is that the getter will return the adjusted value from the query object instead of returning the locally hold base value of the Creature instance
--- chain_of_responsability/message_broker/message_broker.cpp
+++ chain_of_responsability/message_broker/message_broker.cpp
@@ -1,6 +1,100 @@
 #include <iostream>
+#include <string>
+#include <boost/signals2.hpp>
+
+struct Query {
+	std::string m_creatureName; // forward declaration
+	enum Argument { // definition of what can be queryed
+		attack,
+		defence
+	} m_argument; // define and create member of the enum at the same time
+public:
+	int m_result; // this is the adjusted data by the query - the most recent data is safed within the query, NOT within the instances itself
+
+	Query(const std::string& creatureName, Argument arg, int result) :
+		m_result(result),
+		m_argument(arg),
+		m_creatureName(creatureName) 
+	{}
+};
+
+struct Game { // mediator - this is our message broker 
+public:
+	boost::signals2::signal<void(Query&)> m_querys;// define a signal within the game that all instances within the game can subscribe to
+		// the signal is what all instances within the game can subscribe to
+};
+
+struct Creature {
+	Game& m_game; // all other instances within the game can adjust querys that are made within the game that affects a Creature instance
+	int m_attack; // this is only the baseline data - they can be modifyed by a modifyer but these are stay the same though out the game
+	int m_defence;
+	std::string m_name;
+public:
+	Creature(Game& game, int attack, int defence, const std::string& name) :
+		m_game(game),
+		m_attack(attack),
+		m_defence(defence),
+		m_name(name) {}
+
+	int getAttack() const { // here we access the actual data from the query and not the data from within the instance itself since these data is only the baseline value and not the value within the game (which depends on losely coupled modifyers)
+		Query q(m_name, Query::Argument::attack, m_attack); // m_result will be the modifyer adjusted value of the base attack value of this Creature instance
+		m_game.m_querys(q); // here we invoke the signal with our query from the creature to send a message to the mediator/message broker
+			// m_game.m_querys(q) invokes the signal with the Query object q - this invokes all subscribed lambda functions (of the modifyers) which take the Query instance q into account
+		return q.m_result;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Creature& creature) {
+		os << "creature name = " << creature.m_name << "; attack = " << creature.getAttack(); // here the query is done every time when the << operator is called
+		return os;
+	}
+};
+
+// basic COR class - just to have the basic functionallity clustered as a base class
+struct CreatureModifyer { // the modifyer listens to the message broker (aka the game object) on new messages on our m_querys topic
+	Game& m_game;
+	Creature& m_creature;
+public:
+	CreatureModifyer(Game& game, Creature& creature) : // linking of the modifyer to the message broker and the concrete creature
+		m_game(game),
+		m_creature(creature) 
+	{}
+	virtual ~CreatureModifyer() = default;
+};
+
+// differenciations of the modifyers
+class DoubleAttackModifyer : public CreatureModifyer {
+	boost::signals2::connection m_conn;
+
+public:
+	DoubleAttackModifyer(Game& game, Creature& creature) :
+		CreatureModifyer(game, creature) 
+	{
+		m_conn = game.m_querys.connect(
+			[&](Query& q) // CAUTION: not a const Query& because we modify the m_result of the query
+			{
+				if (q.m_creatureName == creature.m_name &&
+						q.m_argument == Query::Argument::attack) 
+				{
+					q.m_result *= 2;
+				}
+			}
+		);
+	}
+
+	~DoubleAttackModifyer() {
+		m_conn.disconnect(); // unsubscribe on destruction
+	}
+};
+
 
 int main() {
-	std::cout << "Hello World" << std::endl;
+	Game game;
+	Creature goblin{ game, 2, 2, "Strong Goblin" };
+	std::cout << goblin << std::endl;
+	{
+		DoubleAttackModifyer dam(game, goblin); // as long as the dam is in scope, all querys (called by .getAttack() within the << operator) will be done with the dam invoked
+		std::cout << goblin << std::endl;
+	}
+	std::cout << goblin << std::endl;
 	return 0;
 }
