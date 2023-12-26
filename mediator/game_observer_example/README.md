05417235fd2173a0db3edf8a00942cc68d21be1b
Jens Weber <jweber1094@gmail.com>
2023-12-26 16:27:28 +0100

    Add game mediator example by using the observer pattern with boost::signals2 as the communication mechanism (instead of implementing communication functions on you own)

--- mediator/game_observer_example/README.md
+++ mediator/game_observer_example/README.md
@@ -1,2 +1,5 @@
 # Game Mediator Example
-+ 
++ In this example, the communication between the objects is done with an observer pattern, implemented with `boost::signals2`!
+	- The mediator has is common task, which is to deliver a communication mechanism. This is done by being able to fire events.
+	- The events are triggered by the objects that connect to the mediator (which is in this case, just by holding the reference to the game object)
+		* The API for fireing the is events is implemented by `boost::signals2`
--- mediator/game_observer_example/game_mediator_example.cpp
+++ mediator/game_observer_example/game_mediator_example.cpp
@@ -1,7 +1,73 @@
 #include <iostream>
+#include <string>
+#include <boost/signals2.hpp>
+
+struct EventData {
+	virtual void print() const = 0;
+};
+
+struct PlayerScoreData : public EventData {
+	std::string player_name;
+	int goals_scored_so_far;
+
+	PlayerScoreData(const std::string& playerName, int goalsSoFar) :
+		player_name(playerName),
+		goals_scored_so_far(goalsSoFar)
+	{}
+
+	void print() const override {
+		std::cout << player_name << " has scored (their " << goals_scored_so_far << " goal)" << std::endl;
+	}
+};
+
+struct Game { // every player and the football coach has a reference to the mediator object, which is in this example the game object
+	boost::signals2::signal<void(EventData*)> events;
+};
+
+struct Player {
+	Game& game;
+	std::string player_name;
+	int goals_so_far{ 0 };
+
+	Player(const std::string& name, Game& game) :
+		player_name(name),
+		game(game)
+	{}
+
+	void score() {
+		goals_so_far++;
+		PlayerScoreData goalData{ player_name, goals_so_far };
+		game.events(&goalData);
+	}
+};
+
+struct Coach {
+	Game& game;
+
+	Coach(Game& game) : game(game) 
+	{
+		game.events.connect(
+			[](EventData* e) 
+			{
+				PlayerScoreData* ps = dynamic_cast<PlayerScoreData*>(e);
+				if (ps != nullptr) { // cast to subclass from abstract base class successfully
+					if (ps->goals_scored_so_far < 3) {
+						std::cout << "Coach says: well done, " << ps->player_name << std::endl;
+					}
+				}
+			}
+		);
+	}
+};
 
 int main() {
+	Game game;
+	Player player1("sam", game);
+	Coach coach(game);
 
-	std::cout << "Hello World!\n";
+	player1.score();
+	player1.score();
+	player1.score(); // no congats after two goals from the coach
+
 	return 0;
 }
