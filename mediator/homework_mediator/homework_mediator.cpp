3f85de5ce6e7a5628473e3292e6b4fb6f398b45e
Jens Weber <jweber1094@gmail.com>
2023-12-26 17:32:58 +0100

    Finish mediator homework

--- mediator/homework_mediator/homework_mediator.cpp
+++ mediator/homework_mediator/homework_mediator.cpp
@@ -1,5 +1,52 @@
-int main() {
+#include <iostream>
+#include <vector>
 
-	std::cout << "Hello World\n";
-	return 0;
+struct IParticipant {
+    virtual void say(int value) = 0;
+    virtual void addValue(int valToAdd) = 0;
+};
+
+struct Mediator {
+    std::vector<IParticipant*> participants;
+    Mediator() = default;
+    void broadcast(int value, IParticipant* sender) {
+        for (auto p : participants) {
+            if (sender != p) { // broadcast to all but not to the sender himself
+                p->addValue(value);
+            }
+        }
+    }
+};
+
+struct Participant : IParticipant
+{
+    int value{ 0 };
+    Mediator& mediator;
+
+    Participant(Mediator& mediator) : mediator(mediator)
+    {
+        mediator.participants.push_back(this);
+    }
+
+    void say(int value) override
+    {
+        mediator.broadcast(value, this);
+    }
+
+    void addValue(int valToAdd) override {
+        value += valToAdd;
+    }
+};
+
+int main() {
+    Mediator med;
+    Participant p1(med);
+    Participant p2(med);
+
+    p1.say(3);
+    std::cout << "p1.value is " << p1.value << ", p2.value is " << p2.value << "; expecting (0, 3) as the correct result" << std::endl;
+    p2.say(2);
+    std::cout << "p1.value is " << p1.value << ", p2.value is " << p2.value << "; expecting (2, 3) as the correct result" << std::endl;
+    
+    return 0;
 }
